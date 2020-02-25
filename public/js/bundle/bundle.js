93ba26b27235f00cc8b9e2c3eb47283c44cec151
Sahak <hakobyan.sahak88@gmail.com>
2020-02-25 21:45:18 +0400

    up

--- public/js/bundle/bundle.js
+++ public/js/bundle/bundle.js
@@ -1,6 +1,28 @@
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+require('./bootstrap');
+
+window.Vue = require('vue');
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('passport-clients', require('./components/passport/Clients.vue')["default"]);
+Vue.component('passport-authorized-clients', require('./components/passport/AuthorizedClients.vue')["default"]);
+Vue.component('passport-personal-access-tokens', require('./components/passport/PersonalAccessTokens.vue')["default"]);
+var app = new Vue({
+  el: '#app'
+});
 /*! Select2 4.0.6-rc.1 | https://github.com/select2/select2/blob/master/LICENSE.md */
+
 !function (a) {
   "function" == typeof define && define.amd ? define(["jquery"], a) : "object" == (typeof module === "undefined" ? "undefined" : _typeof(module)) && module.exports ? module.exports = function (b, c) {
     return void 0 === c && (c = "undefined" != typeof window ? require("jquery") : require("jquery")(b)), a(c), c;
