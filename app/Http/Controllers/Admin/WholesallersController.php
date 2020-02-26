25e49d03e60b73a6739b01029659316076ca5fa5
Sahak <hakobyan.sahak88@gmail.com>
2020-02-26 15:44:34 +0400

    up

--- app/Http/Controllers/Admin/WholesallersController.php
+++ app/Http/Controllers/Admin/WholesallersController.php
@@ -41,6 +41,6 @@ class WholesallersController extends Controller
 
     public function synch(WholesaleService $service)
     {
-        dd($service->getAllItems());
+        dd($service->getAllItems()[0]);
     }
 }
