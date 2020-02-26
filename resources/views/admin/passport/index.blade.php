28266df36ac9cae079869e9cc4977d7633c11651
Sahak <hakobyan.sahak88@gmail.com>
2020-02-26 11:21:50 +0400

    up

--- resources/views/admin/passport/index.blade.php
+++ resources/views/admin/passport/index.blade.php
@@ -4,11 +4,11 @@
 @stop
 @section('content')
     <div id="app">
-    <passport-clients></passport-clients>
+        <passport-clients></passport-clients>
 
-    <passport-authorized-clients></passport-authorized-clients>
+        <passport-authorized-clients></passport-authorized-clients>
 
-    <passport-personal-access-tokens></passport-personal-access-tokens>
+        <passport-personal-access-tokens></passport-personal-access-tokens>
     </div>
 @stop
 
@@ -16,4 +16,4 @@
     <!-- Styles -->
     <script src="{{ asset('public/js/app.js') }}" defer></script>
     <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
-    @stop
+@stop
