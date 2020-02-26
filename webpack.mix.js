eb6b8b088d17982130caa6cde046a2397561537b
Sahak <hakobyan.sahak88@gmail.com>
2020-02-26 11:24:30 +0400

    up

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,44 +10,46 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-// mix.js('resources/js/app.js', 'public/js')
-//     .sass('resources/sass/app.scss', 'public/css');
 
-mix.babel([
-    'public/js/jQuery3.3.1.js',
-    'public/plugins/jquery-ui/jquery-ui.min.js',
-], 'public/js/bundle/jquery.js')
-    .babel([
-        'public/plugins/select2/select2.full.min.js',
-        'public/js/bootstrap.bundle.min.js',
-        'public/js/hover-slider.js',
-        'public/js/main.js',
-        'public/js/util.js',
-        'public/js/register.js',
-        'public/js/login.js',
-        'public/js/adult.js'
-    ], 'public/js/bundle/bundle.js')
-    // .babel([
-    //     'public/js/media_button_new.js'
-    // ], 'public/js/bundle/media.js')
-    .babel([
-        'public/js/bootstrap-fileinput/js/fileinput.min.js',
-        'public/plugins/formstone/mediaquery.js',
-        'public/plugins/formstone/touch.js',
-        'public/plugins/formstone/carousel/carousel.js',
-        'public/js/welcome_carousel.js'
-    ], 'public/js/bundle/carousel.js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .styles([
-        'public/css/app.css',
-        'public/css/custom.css',
-        'public/css/fonts.css',
-        'public/frontend/css/bootstrap.min.css',
-        'public/plugins/jquery-ui/jquery-ui.min.css',
-        'public/plugins/select2/select2.min.css',
-        'public/css/global.css',
-        'public/css/products.css',
-        'public/css/product-cards.css',
-        'public/css/main.css',
-        'public/css/flag-icon.css'
-    ], 'public/css/bundle.css')
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
+
+
+// mix.babel([
+//     'public/js/jQuery3.3.1.js',
+//     'public/plugins/jquery-ui/jquery-ui.min.js',
+// ], 'public/js/bundle/jquery.js')
+//     .babel([
+//         'public/plugins/select2/select2.full.min.js',
+//         'public/js/bootstrap.bundle.min.js',
+//         'public/js/hover-slider.js',
+//         'public/js/main.js',
+//         'public/js/util.js',
+//         'public/js/register.js',
+//         'public/js/login.js',
+//         'public/js/adult.js'
+//     ], 'public/js/bundle/bundle.js')
+//     // .babel([
+//     //     'public/js/media_button_new.js'
+//     // ], 'public/js/bundle/media.js')
+//     .babel([
+//         'public/js/bootstrap-fileinput/js/fileinput.min.js',
+//         'public/plugins/formstone/mediaquery.js',
+//         'public/plugins/formstone/touch.js',
+//         'public/plugins/formstone/carousel/carousel.js',
+//         'public/js/welcome_carousel.js'
+//     ], 'public/js/bundle/carousel.js')
+//     .sass('resources/sass/app.scss', 'public/css')
+//     .styles([
+//         'public/css/app.css',
+//         'public/css/custom.css',
+//         'public/css/fonts.css',
+//         'public/frontend/css/bootstrap.min.css',
+//         'public/plugins/jquery-ui/jquery-ui.min.css',
+//         'public/plugins/select2/select2.min.css',
+//         'public/css/global.css',
+//         'public/css/products.css',
+//         'public/css/product-cards.css',
+//         'public/css/main.css',
+//         'public/css/flag-icon.css'
+//     ], 'public/css/bundle.css')
