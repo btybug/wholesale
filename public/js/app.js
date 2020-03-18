2cfb68dc8869e6259a860c21e3481635fcd79686
Terakyan <edo.terakyan@gmail.com>
2020-03-18 19:52:24 +0400

    up

--- public/js/app.js
+++ public/js/app.js
@@ -52513,8 +52513,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\vape\wholesale\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\vape\wholesale\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\wholesale\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\wholesale\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
